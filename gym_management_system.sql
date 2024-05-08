749ddb70c2386254e1b622529b959c9bbe70cfd5
Thanuja Dilshan <148608474+thanujadilshan16863@users.noreply.github.com>
2024-05-09 00:40:56 +0530

    Update gym_management_system.sql
    td

--- gym_management_system.sql
+++ gym_management_system.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 24, 2024 at 07:27 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: May 08, 2024 at 09:10 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,50 @@ SET time_zone = "+00:00";
 --
 -- Database: `gym management system`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `NIC` int(11) DEFAULT NULL,
+  `Customer_ID` int(8) NOT NULL,
+  `First_N` varchar(31) NOT NULL,
+  `Last_N` varchar(33) NOT NULL,
+  `Address` varchar(110) NOT NULL,
+  `Mobile` int(10) NOT NULL,
+  `Emergency_Name` varchar(30) NOT NULL,
+  `Emergency_Contact` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`NIC`, `Customer_ID`, `First_N`, `Last_N`, `Address`, `Mobile`, `Emergency_Name`, `Emergency_Contact`) VALUES
+(12312312, 1, 'Thanuja', 'nawa', 'adsfad', 3123123, 'wsdfsdf', 312312);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `Customer_ID` int(8) NOT NULL,
+  `Date` int(11) NOT NULL DEFAULT current_timestamp(),
+  `Type` varchar(11) NOT NULL,
+  `Amount` int(17) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `payments`
+--
+
+INSERT INTO `payments` (`Customer_ID`, `Date`, `Type`, `Amount`) VALUES
+(1, 5092024, 'Monthly', 3500);
 
 -- --------------------------------------------------------
 
@@ -40,6 +84,19 @@ CREATE TABLE `users` (
 --
 
 --
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`Customer_ID`),
+  ADD UNIQUE KEY `NIC` (`NIC`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`Customer_ID`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -48,6 +105,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `Customer_ID` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
